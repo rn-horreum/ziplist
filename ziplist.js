d84ae182937042b1025fe07158076b7cce2d45bf
Roman Nicolai <54918828+romannicolai@users.noreply.github.com>
2019-09-23 20:48:15 -1000

    Update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -1,2 +1,22 @@
+const testList1 = ['a', 'b', 'c'];
+const testList2 = [1, 2, 3];
+
+function zipList(list1, list2) {
+  const resultList = [];
+  for (let i = 0; i < list1.length; i++) {
+    resultList.push(list1[i], list2[i]);
+  }
+  return resultList;
+}
+
+console.log(zipList(testList1, testList2));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(testList1, testList2));
+
+
 
 
